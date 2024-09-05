71d03695d1ace1cf89fbb3a75c0cc51e1385c618
Fernando <fcanto08@gmail.com>
2024-09-05 11:32:15 -0300

    Nprimitivos

---
+++ Unidade 01/Aula 02/ExemplosDadosNPrimitivo.java
@@ -0,0 +1,30 @@
+public class ExemplosDadosNPrimitivo {
+    public static void main(String [] args){
+        // tipos caracter PRIMITIVO
+        char caracter = 'A' ;
+
+
+        // tipo caracteres Não PRIMITIVO
+        String nome = "Fernando";
+        nome = "Fernando Canto";
+
+
+        // tipo Enum
+        enum DiaDaSemana{
+            DOMINGO, SEGUNDA, TERÇA, QUARTA, QUINTA, SEXTA, SÁBADO
+        }
+
+
+        // tipo array
+        int [] numeros = {1,2,3,4,5}; // começa sempre em 0,1,2,3,4
+
+
+        DiaDaSemana dia = DiaDaSemana.SÁBADO;
+        System.out.println(nome);
+        System.out.println(dia);
+        System.out.println(numeros [0]);
+    }
+
+
+}
+

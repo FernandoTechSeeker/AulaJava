428f353f2c837d13a670834ec1e4079936dc716c
Fernando <fcanto08@gmail.com>
2024-09-09 16:45:06 -0300

    VariaveisPrimitivas

---
+++ Unidade 01/Exercicio 01/VariaveisPrimitivas.java
@@ -0,0 +1,19 @@
+public class VariaveisPrimitivas {
+    
+    public static void main(String[] args) {
+
+        int numerointeiro = 10;
+        float numeroDecimal = 1.8f;
+        char caractere = 'A';
+        boolean valorBooleano = true;
+
+        System.out.println("Número inteiro: " + numerointeiro);
+        System.out.println("Número decimal: " + numeroDecimal);
+        System.out.println("Caractere: " + caractere);
+        System.out.println("Valor booleano: " + valorBooleano);
+
+
+
+        
+    }
+}

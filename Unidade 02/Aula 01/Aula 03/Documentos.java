a149c18bab7061de5eaeba78a60111fb6b2f0b81
Fernando <fcanto08@gmail.com>
2024-09-11 10:22:29 -0300

    Documentos

--- Unidade 02/Aula 01/Aula 03/Documentos.java
+++ Unidade 02/Aula 01/Aula 03/Documentos.java
@@ -1,3 +1,42 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Documentos {
-    
+    //Atributos dos documentos
+    private String titulo;
+    private int nPaginas;
+
+    //construtor
+    public Documentos(String titulo, int nPaginas) {
+        this.titulo = titulo;
+        this.nPaginas = nPaginas;
+    }
+
+    public static void main(String[] args) {
+        // ter certeza do que esta fazendo
+        Deque<Documentos> pilha = new ArrayDeque<>();
+
+        Documentos doc = new Documentos("Processo 123", 100);
+        Documentos doc2 = new Documentos("Processso 456" , 240);
+        Documentos doc3 = new Documentos("Processso 789" , 675);
+        Documentos doc4 = new Documentos("Processso 321" , 45);
+        Documentos doc5 = new Documentos("Processso 654" , 72);
+
+        pilha.push(doc);
+        pilha.push(doc2);
+        pilha.push(doc3);
+        pilha.push(doc4);
+        pilha.push(doc5);
+
+        while(!pilha.isEmpty()){     //ei não precisa do cast, tendo certeza do que colocou do DEQUE
+            Documentos tempDocumentos = (Documentos)pilha.pop();
+            System.out.println("Titulo:   " + tempDocumentos.titulo);
+            System.out.println("Páginas:  " + tempDocumentos.nPaginas);
+            System.out.println("");
+
+        }
+
+        
+
+    }
 }

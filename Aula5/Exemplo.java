43e7bbb8406349acd43a94d2b11d47eaa243abf2
Fernando <fcanto08@gmail.com>
2024-10-31 12:19:58 -0300

    initial commit

---
+++ Aula5/Exemplo.java
@@ -0,0 +1,13 @@
+package Aula5;
+
+public class Exemplo {
+
+    public static void main(String[] args) {
+        Carro carro = new Carro("Ferrari", "F50", 2);
+
+        carro.Acelerar();
+        carro.Abastecer();
+        carro.Abastecer(35);;
+    }
+    
+}
